9771d4249fbefd9a8985cccf6c0c2a8ba91bb05d
ssoheily <sam.soheil55@gmail.com>
2021-08-10 13:33:07 +0200

    prime number

--- myscript.js
+++ myscript.js
@@ -1,1 +1,29 @@
-document.write("hi world");
+// document.write("hi world");
+let isPrime = true;
+
+let n = 11	;
+
+let i = 2;
+
+// while(i<n){
+// 	if(n % i == 0){
+
+// 		isPrime = false
+// 		// console.log(i);
+// 		break;
+// 	}
+// 	i++;
+// }
+
+// prime number ????
+while(i< parseInt( n ** 0.5 ) + 1 ){
+	if(n % i == 0){
+
+		isPrime = false
+		console.log(i);
+		break;
+
+	}
+	i++;
+}
+console.log(n,isPrime);
